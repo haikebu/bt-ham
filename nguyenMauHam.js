ffb2c95bc301e6ea75e097ccdb3be790892293a6
Haikebu <bhhai1610@gmail.com>
2021-04-15 20:43:59 +0700

    hàm tính tuổi 1.0

--- nguyenMauHam.js
+++ nguyenMauHam.js
@@ -1,15 +1,20 @@
-//ham tinhTuoi
+//Hàm tính tuổi của người dùng
 //tham số: birthYear là 1 số năm sinh người dùng nhập vào
 //trả về 1 số là số tuổi
-function tinhTuoi(birthYear) {
+function findAge(birthYear) {
 }
 //Hàm tìm vị trí số lớn nhất trong một mảng
-//tham số: agg là 1 mảng 
+//tham số: arr là 1 mảng 
 //trả về 1 số là vị trí của giá trị lớn nhất trong mảng
-function findMax(agg) {
+function findMax(arr) {
 }
 //Hàm tính bình phương của một số
 //Tham số: n là 1 số
 //Trả về: là 1 số bằng bình phương của n
 function square(n){
+}
+//Hàm tính nghiệm của phương trình bậc 2: ax^2 + bx + c = 0
+//Tham số: a,b,c là 1 mảng chứa 3 giá trị a,b,c của phương trình
+//Trả về: là 1 mảng chứa các nghiệm của phương trình
+function findRootsOfSecondDegreeEquation(arrABC){   
 }
