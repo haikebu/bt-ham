1694ce4aed69b66ae7bc98b48ef6c1bd1ef02018
Haikebu <bhhai1610@gmail.com>
2021-04-15 21:02:32 +0700

    2.0

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function findAge() {
+function tinhKetQua() {
+    let birthYear = document.getElementById("birthYear").value
+    findAge(birthYear)
+    document.getElementById("age").innerHTML = 'Tuổi của bạn là:' + age
+}
+function findAge(birthYear) {
     let today = new Date();
-    let year = document.getElementById("birthday").value 
-    let currentYear = today.getFullYear() 
-    age = currentYear - year
-    document.getElementById("display").innerText = "Tuổi của bạn là:"
-    document.getElementById("age").innerText = age
+    let currentYear = today.getFullYear()
+    age = currentYear - birthYear
     return age
 }
